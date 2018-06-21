594cb4b32bb19c71be943c6c85996db599374292
Emily Wessel <ettl02@gmail.com>
2018-06-21 01:58:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length == 1
-    return "#{array[0]}"
+    "#{array[0]}"
   elsif array.length == 2
-    return array.join(" and ")
+    array.join(" and ")
   elsif array.length >= 3
     array[-1] = "and #{array[-1]}"
-    return array.join(", ")
+    array.join(", ")
   end
 end
